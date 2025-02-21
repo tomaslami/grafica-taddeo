caf98df69dff15e1ff1182fc09a4250adaf4e8aa
tomaslami <tomaslamiguralnik@gmail.com>
2025-02-21 18:11:57 -0300

    ultimos detalles

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["tudominio.vercel.app"], // Agrega el dominio de tu despliegue en Vercel
-    formats: ["image/webp", "image/avif"],
-    unoptimized: false, // Asegura que Next.js optimice las imágenes
-  },
-  trailingSlash: true, // Puede ayudar con rutas incorrectas en producción
-  reactStrictMode: true, // Mantiene el código más seguro y detecta errores
-  swcMinify: true, // Optimización del código
-  output: "standalone", // Asegura que la app funcione bien en diferentes entornos
+  /* config options here */
 };
 
 export default nextConfig;
