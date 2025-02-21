016074f883acc82b4ee8143d6fc2b9db25c98e6c
tomaslami <tomaslamiguralnik@gmail.com>
2025-02-21 17:16:41 -0300

    cambio de imagen, impresion digital

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -37,7 +37,7 @@ const services = [
   },
   {
     title: 'Impresión Digital',
-    image: '/impresion-digital/impresion1.png',
+    image: '/impresion-offset/impresion1.png',
     href: '/impresion-digital',
     gridClass: 'md:col-span-1 md:row-span-1'
   },
